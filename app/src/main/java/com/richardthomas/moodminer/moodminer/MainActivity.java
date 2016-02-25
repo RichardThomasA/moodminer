8ff201a4f130e201e0c105f90482841d9222acca
Richard Thomas <richard.t.alappat@gmail.com>
2016-02-25 14:36:12 +0530

    started adding login functionality

--- app/src/main/java/com/richardthomas/moodminer/moodminer/MainActivity.java
+++ app/src/main/java/com/richardthomas/moodminer/moodminer/MainActivity.java
@@ -2,13 +2,40 @@ package com.richardthomas.moodminer.moodminer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton=(Button)findViewById(R.id.button);
+
+        username= (EditText) findViewById(R.id.editText);
+        password= (EditText) findViewById(R.id.editText2);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("root")&&
+                        password.getText().toString().equals("root")){
+                    //if_body
+                    //if the user is valid
+                    Toast.makeText(MainActivity.this, "Login successful...", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Login unsuccessful.Try again...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
